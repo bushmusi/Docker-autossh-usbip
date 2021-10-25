1926f08c3099c93832813d5bc003b96fba632ddf
bushmusi <bushera.mestofa@gmail.com>
2021-10-25 12:43:19 +0300

    Dockerize auto ssh and usbip

--- docker-autossh/entrypoint.sh
+++ docker-autossh/entrypoint.sh
@@ -3,4 +3,4 @@
 touch /id_rsa
 chmod 0400 /id_rsa
 
-autossh -M 0 -N -q  -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R 3240:localhost:3240 remoteuser@remoteserver.com
+autossh -M 0 -N -q  -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R 3240:localhost:3240 root@172.17.0.3
